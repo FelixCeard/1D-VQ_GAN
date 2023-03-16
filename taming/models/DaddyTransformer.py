d1357bf5ea71af917efd54dd402be75f0e1b977d
Felix Ceard-Falkenberg <fece00001@stud.uni-saarland.de>
2023-03-16 12:31:23 +0100

    fixxinggg

--- taming/models/DaddyTransformer.py
+++ taming/models/DaddyTransformer.py
@@ -73,6 +73,7 @@ class DaddyTransformer(pl.LightningModule):
 
 		with torch.no_grad():
 			logits= self(x)
+			print(logits.shape, x.shape, y.shape)
 			loss = F.cross_entropy(logits.reshape(-1), y.reshape(-1))
 			# loss = self.transformer.shared_step(batch, batch_idx)
 			self.log("val/Transloss", loss, prog_bar=False, logger=True, on_step=True, on_epoch=True)
